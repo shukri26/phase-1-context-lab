6e6ac150de6aaff0296026d1a489b0492e4fcb14
Tim <mohamud.hussein@student.moringaschool.com>
2023-07-10 06:58:46 +0300

    finished

--- index.js
+++ index.js
@@ -1,4 +1,81 @@
 /* Your Code Here */
+function createEmployeeRecord(arr) {
+    const employee = {
+      firstName: arr[0],
+      familyName: arr[1],
+      title: arr[2],
+      payPerHour: arr[3],
+      timeInEvents: [],
+      timeOutEvents: []
+    };
+    return employee;
+  };
+  function createEmployeeRecords(arr){
+    const createEmployeeRecords = arr.map(EmployeeData=>createEmployeeRecord(EmployeeData))
+    return createEmployeeRecords;
+  };
+  
+  const createTimeInEvent = function (dateStamp) {
+    let [date, hour] = dateStamp.split(" ");
+    this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date,
+    });
+    return this;
+  };
+
+const createTimeOutEvent = function (dateStamp) {
+  let [date, hour] = dateStamp.split(" ");
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  });
+  return this;
+};
+  const hoursWorkedOnDate = function (appropriateDate) {
+    let inEvent = this.timeInEvents.find((event) => {
+      return event.date === appropriateDate;
+    });
+    let outEvent = this.timeOutEvents.find(function (event) {
+      return event.date === appropriateDate;
+    });
+    return (outEvent.hour - inEvent.hour) / 100;
+  };
+  const wagesEarnedOnDate = function (appropriateDate) {
+    let rawSalary =
+      hoursWorkedOnDate.call(this, appropriateDate) * this.payPerHour;
+    return parseFloat(rawSalary.toString());
+  };
+  
+  let allWages = function () {
+    let appropriateDates = this.timeInEvents.map(function (e) {
+      return e.date;
+    });
+  
+    let payable = appropriateDates.reduce(
+      function (memo, date) {
+        return memo + wagesEarnedOnDate.call(this, date);
+      }.bind(this),
+      0
+    );
+  
+    return payable;
+  };
+  
+  const findEmployeeByFirstName = function (arr, firstName) {
+    return arr.find(function (records) {
+      return records.firstName === firstName;
+    });
+  };
+  
+  const calculatePayroll = function (arrayOfEmployeeRecords) {
+    return arrayOfEmployeeRecords.reduce(function (memo, records) {
+      return memo + allWages.call(records);
+    }, 0);
+  };
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +97,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
